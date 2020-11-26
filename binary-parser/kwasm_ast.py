75c1357eba76e1e90079ff16c60851ff28c35f82
hjorthjort <rikard.hjort@gmail.com>
2020-11-26 17:40:07 +0000

    Bugfix/workaround
    The string representation of bytes that Kast needs is hard to achieve in Python.
    This is a workaround to that.
    It may be more brittle than necessary, so suggestions are welcome.

--- binary-parser/kwasm_ast.py
+++ binary-parser/kwasm_ast.py
@@ -68,7 +68,16 @@ def KFloat(value : float):
     return KToken(str(value), 'Float')
 
 def KBytes(bs : bytes):
-    return KToken(str(bs), 'Bytes')
+    # Hack around the fact that Python gives no easy way to represent bytes as a string in a way the K frontend accepts.
+    # Assume bs = bytes([0, 65, 255])
+    # We want ret = "b'\x00A\xff".
+    # str(bs) = "b'\\x00A\\xff"
+    # bs.decode() throws error, because \xff is not a valid Unicode byte.
+    # bs.decode(errors='backslashescape') = "b'\x00A\\xff" which is alright for bytes below \x7f, but for higher bytes, the fully escaped backslash shows up again.
+    # For now, we just pipe it through a helper function. This approach might be brittle.
+    i = int.from_bytes(bs, 'little')
+    ret = KApply('Int2Bytes(_,_,_)_BYTES-HOOKED_Bytes_Int_Endianness_Signedness', [KInt(i), KApply('littleEndianBytes', []), KApply('unsignedBytes', [])])
+    return ret
 
 ###########
 # Strings #
