c72fbb81e12afc6eb331af1909ff8d078b98963f
hjort <git@hjorthjort.xyz>
2021-04-22 14:52:56 +0000

    use krunJSONLegacy

--- binary-parser/test.py
+++ binary-parser/test.py
@@ -12,7 +12,7 @@ def config_to_kast_term(config):
 def run_module(parsed_module):
     input_json = config_to_kast_term(parsed_module)
     krun_args = [ '--term', '--debug']
-    (rc, new_wasm_config, err) = pyk.krunJSON(WASM_definition_llvm_no_coverage_dir, input_json, krunArgs = krun_args, teeOutput=True)
+    (rc, new_wasm_config, err) = pyk.krunJSONLegacy(WASM_definition_llvm_no_coverage_dir, input_json, krunArgs = krun_args, teeOutput=True)
     if rc != 0:
         raise Exception("Received error while running: " + err )
 
