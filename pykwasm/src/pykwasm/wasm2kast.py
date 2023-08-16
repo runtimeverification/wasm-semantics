51b9d18625ad764a784f6bca985ca57926d8e903
Burak Bilge Yalçınkaya <bbyalcinkaya@gmail.com>
2023-08-16 14:03:46 +0300

    Move binary parser to pykwasm (#489)
    * move source and test files
    
    * symlink to py-wasm source
    
    * delete old binary-parser files
    
    * wasm2kast in pykwasm
    
    * formatting
    
    * pykwasm unit and integration tests
    
    * formatting
    
    * kwasm_ast.py type annot
    
    * cleanup test flags
    
    * use cloned py-wasm
    
    * install py-wasm from tar.gz

--- pykwasm/src/pykwasm/wasm2kast.py
+++ pykwasm/src/pykwasm/wasm2kast.py
@@ -5,12 +5,34 @@ This library provides a translation from the Wasm binary format to Kast.
 """
 
 import sys
-import json
-import kwasm_ast as a
+from typing import IO, Iterable
 
-from wasm.parsers.module import parse_module, Module
-from wasm.datatypes import ValType, TypeIdx, FunctionType, Function, Table, TableType, Memory, MemoryType, Limits, Global, GlobalType, Mutability, ElementSegment, DataSegment, StartFunction, Import, Export
+from pyk.kast.inner import KInner
+from wasm.datatypes import (
+    DataSegment,
+    ElementSegment,
+    Export,
+    Function,
+    FunctionType,
+    Global,
+    GlobalType,
+    Import,
+    Limits,
+    Memory,
+    MemoryType,
+    Module,
+    Mutability,
+    StartFunction,
+    Table,
+    TableType,
+    TypeIdx,
+    ValType,
+)
 from wasm.opcodes import BinaryOpcode
+from wasm.parsers import parse_module
+
+from pykwasm import kwasm_ast as a
+
 
 def main():
     if len(list(sys.argv)) == 1:
@@ -21,67 +43,91 @@ def main():
     infile.close()
     return module
 
-def wasm2kast(wasm_bytes : bytes, filename=None) -> dict:
+
+def wasm2kast(wasm_bytes: IO[bytes], filename=None) -> KInner:
     """Returns a dictionary representing the Kast JSON."""
     ast = parse_module(wasm_bytes)
     return ast2kast(ast, filename=filename)
 
-def ast2kast(wasm_ast : Module, filename=None) -> dict:
+
+def ast2kast(wasm_ast: Module, filename=None) -> KInner:
     """Returns a dictionary representing the Kast JSON."""
-    types   = a.defns([typ(x)    for x in wasm_ast.types])
-    funcs   = a.defns([func(x)   for x in wasm_ast.funcs])
-    tables  = a.defns([table(x)  for x in wasm_ast.tables])
-    mems    = a.defns([memory(x) for x in wasm_ast.mems])
-    globs   = a.defns([glob(x)   for x in wasm_ast.globals])
-    elems   = a.defns([elem(x)   for x in wasm_ast.elem])
-    datas   = a.defns([data(x)   for x in wasm_ast.data])
-    starts  = a.defns(start(wasm_ast.start))
-    imports = a.defns([imp(x)    for x in wasm_ast.imports])
+    types = a.defns([typ(x) for x in wasm_ast.types])
+    funcs = a.defns([func(x) for x in wasm_ast.funcs])
+    tables = a.defns([table(x) for x in wasm_ast.tables])
+    mems = a.defns([memory(x) for x in wasm_ast.mems])
+    globs = a.defns([glob(x) for x in wasm_ast.globals])
+    elems = a.defns([elem(x) for x in wasm_ast.elem])
+    datas = a.defns([data(x) for x in wasm_ast.data])
+    starts = a.defns(start(wasm_ast.start))
+    imports = a.defns([imp(x) for x in wasm_ast.imports])
     exports = a.defns([export(x) for x in wasm_ast.exports])
-    meta    = a.module_metadata(filename=filename)
-    return a.module(types=types, funcs=funcs, tables=tables, mems=mems, globs=globs, elem=elems, data=datas, start=starts, imports=imports, exports=exports, metadata=meta)
+    meta = a.module_metadata(filename=filename)
+    return a.module(
+        types=types,
+        funcs=funcs,
+        tables=tables,
+        mems=mems,
+        globs=globs,
+        elem=elems,
+        data=datas,
+        start=starts,
+        imports=imports,
+        exports=exports,
+        metadata=meta,
+    )
+
 
 #########
 # Defns #
 #########
 
-def typ(t : FunctionType):
+
+def typ(t: FunctionType):
     return a.type(func_type(t.params, t.results))
 
-def func(f : Function):
+
+def func(f: Function):
     type = a.KInt(f.type_idx)
     ls_list = [val_type(x) for x in f.locals]
     locals = a.vec_type(a.val_types(ls_list))
     body = instrs(f.body)
     return a.func(type, locals, body)
 
-def table(t : Table):
+
+def table(t: Table):
     ls = limits(t.type.limits)
     return a.table(ls)
 
-def memory(m : Memory):
+
+def memory(m: Memory):
     ls = limits(m.type)
     return a.memory(ls)
 
-def glob(g : Global):
+
+def glob(g: Global):
     t = global_type(g.type)
     init = instrs(g.init)
     return a.glob(t, init)
 
-def elem(e : ElementSegment):
+
+def elem(e: ElementSegment):
     offset = instrs(e.offset)
     return a.elem(e.table_idx, offset, e.init)
 
-def data(d : DataSegment):
+
+def data(d: DataSegment):
     offset = instrs(d.offset)
     return a.data(d.memory_idx, offset, d.init)
 
-def start(s : StartFunction):
+
+def start(s: StartFunction):
     if s is None:
         return []
     return [a.start(s.function_idx)]
 
-def imp(i : Import):
+
+def imp(i: Import):
     mod_name = a.wasm_string(i.module_name)
     name = a.wasm_string(i.as_name)
     t = type(i.desc)
@@ -95,10 +141,12 @@ def imp(i : Import):
         desc = a.memory_desc(limits(i.desc))
     return a.imp(mod_name, name, desc)
 
-def export(e : Export):
+
+def export(e: Export):
     name = a.wasm_string(e.name)
     idx = e.desc
     return a.export(name, idx)
+
 
 ##########
 # Instrs #
@@ -106,15 +154,15 @@ def export(e : Export):
 
 block_id = 0
 
+
 def instrs(iis):
     """Turn a list of instructions into KAST."""
     # We ignore `END`.
     # The AST supplied by py-wasm has already parsed these and terminated the blocks.
     # We also ignore `ELSE`.
     # The AST supplied by py-wasm includes the statements in the else-branch as part of the `IF` instruction.
-    return a.instrs([instr(i) for i in iis
-                                            if not i.opcode == BinaryOpcode.END
-                                           and not i.opcode == BinaryOpcode.ELSE])
+    return a.instrs([instr(i) for i in iis if not i.opcode == BinaryOpcode.END and not i.opcode == BinaryOpcode.ELSE])
+
 
 def instr(i):
     B = BinaryOpcode
@@ -136,17 +184,17 @@ def instr(i):
     if i.opcode == B.CALL_INDIRECT:
         return a.CALL_INDIRECT(i.type_idx)
     if i.opcode == B.ELSE:
-        raise(ValueError("ELSE opcode: should have been filtered out."))
+        raise (ValueError('ELSE opcode: should have been filtered out.'))
     if i.opcode == B.END:
-        raise(ValueError("End opcode: should have been filtered out."))
+        raise (ValueError('End opcode: should have been filtered out.'))
     if i.opcode == B.F32_CONST:
         return a.F32_CONST(i.value)
     if i.opcode == B.F64_CONST:
         return a.F64_CONST(i.value)
     if i.opcode == B.F32_REINTERPRET_I32:
-        raise(ValueError('Reinterpret instructions not implemented.'))
+        raise (ValueError('Reinterpret instructions not implemented.'))
     if i.opcode == B.F64_REINTERPRET_I64:
-        raise(ValueError('Reinterpret instructions not implemented.'))
+        raise (ValueError('Reinterpret instructions not implemented.'))
     if i.opcode == B.GET_GLOBAL:
         return a.GET_GLOBAL(i.global_idx)
     if i.opcode == B.GET_LOCAL:
@@ -156,9 +204,9 @@ def instr(i):
     if i.opcode == B.I64_CONST:
         return a.I64_CONST(i.value)
     if i.opcode == B.I32_REINTERPRET_F32:
-        raise(ValueError('Reinterpret instructions not implemented.'))
+        raise (ValueError('Reinterpret instructions not implemented.'))
     if i.opcode == B.I64_REINTERPRET_F64:
-        raise(ValueError('Reinterpret instructions not implemented.'))
+        raise (ValueError('Reinterpret instructions not implemented.'))
     if i.opcode == B.IF:
         cur_block_id = block_id
         block_id += 1
@@ -225,14 +273,15 @@ def instr(i):
     if i.opcode == B.TEE_LOCAL:
         return a.TEE_LOCAL(i.local_idx)
     # Catch all for operations without direct arguments.
-    op = i.opcode
     return eval('a.' + i.opcode.name)
+
 
 ########
 # Data #
 ########
 
-def val_type(t : ValType):
+
+def val_type(t: ValType):
     if t == ValType.i32:
         return a.i32
     if t == ValType.i64:
@@ -242,27 +291,24 @@ def val_type(t : ValType):
     if t == ValType.f64:
         return a.f64
 
-def vec_type(ts : [ValType]):
+
+def vec_type(ts: Iterable[ValType]):
     _ts = [val_type(x) for x in ts]
     return a.vec_type(a.val_types(_ts))
+
 
 def func_type(params, results):
     pvec = vec_type(params)
     rvec = vec_type(results)
     return a.func_type(pvec, rvec)
 
-def limits(l : Limits):
+
+def limits(l: Limits):
     return (l.min, l.max)
 
-def global_type(t : GlobalType):
+
+def global_type(t: GlobalType):
     vt = val_type(t.valtype)
     if t.mut is Mutability.const:
         return a.global_type(a.MUT_CONST, vt)
     return a.global_type(a.MUT_VAR, vt)
-
-########
-# Main #
-########
-
-if __name__ == "__main__":
-    main()
