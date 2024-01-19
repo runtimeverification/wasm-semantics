5b651304c5a961945b71bdbca40a23505b9ac844
Burak Bilge Yalcinkaya <bbyalcinkaya@gmail.com>
2024-01-19 03:58:02 +0300

    add table unit test

---
+++ pykwasm/src/tests/unit/test_wasm2kast.py
@@ -0,0 +1,21 @@
+import pytest
+from pyk.kast.inner import KApply, KInner
+from wasm.datatypes import ExternAddress, FunctionAddress, Limits, Table, TableType
+
+from pykwasm import wasm2kast
+from pykwasm.kwasm_ast import KInt, externref, funcref
+
+TABLE_TEST_DATA = (
+    (Table(TableType(Limits(0, None), FunctionAddress)), KApply('limitsMin', [KInt(0)]), funcref),
+    (Table(TableType(Limits(0, 100), ExternAddress)), KApply('limitsMinMax', [KInt(0), KInt(100)]), externref),
+)
+
+
+@pytest.mark.parametrize(('input', 'limits', 'typ'), TABLE_TEST_DATA)
+def test_table(input: Table, limits: KInner, typ: KInner) -> None:
+    # When
+    t = wasm2kast.table(input)
+
+    # Then
+    assert limits == t.args[0]
+    assert typ == t.args[1]
