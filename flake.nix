7582575be6acdada4290c348df5daeb4813539cb
Bruce Collie <brucecollie82@gmail.com>
2024-04-29 22:01:28 +0100

    Working flake

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "K Semantics of WebAssembly";
 
   inputs = {
-    k-framework.url = "github:runtimeverification/k/v6.3.74";
+    k-framework.url = "github:runtimeverification/k/v7.0.25";
     nixpkgs.follows = "k-framework/nixpkgs";
     flake-utils.follows = "k-framework/flake-utils";
     rv-utils.follows = "k-framework/rv-utils";
-    pyk.url = "github:runtimeverification/pyk/v0.1.775";
+    pyk.url = "github:runtimeverification/k/v7.0.25?dir=pyk";
     nixpkgs-pyk.follows = "pyk/nixpkgs";
     poetry2nix.follows = "pyk/poetry2nix";
   };
@@ -26,72 +26,64 @@
 
           nixpkgs-pyk = import inputs.nixpkgs-pyk {
             system = prev.system;
+            overlays = [ pyk.overlay ];
           };
+
+          python310-pyk = nixpkgs-pyk.python310;
 
           poetry2nix = inputs.poetry2nix.lib.mkPoetry2Nix { pkgs = nixpkgs-pyk; };
         in {
-          # kwasm-unwrapped = prev.stdenv.mkDerivation {
-          #   inherit src version;
+          pyk = pyk.packages.${prev.system}.pyk;
 
-          #   pname = "kwasm-unwrapped";
+          kwasm = prev.stdenv.mkDerivation {
+            pname = "kwasm";
+            inherit src version;
 
-          #   dontBuild = true;
+            buildInputs = with final; [
+              k-framework.packages.${system}.k
+              final.kwasm-pyk 
+              python310-pyk
+            ];
 
-          #   installPhase = ''
-          #     mkdir -p $out/bin
-          #     cp ./kwasm $out/bin
-          #     cp ./preprocessor.py $out/bin
-          #   '';
+            nativeBuildInputs = [ prev.makeWrapper ];
 
-          #   fixupPhase = ''
-          #     patchShebangs "$out/bin/kwasm"
-          #     substituteInPlace "$out/bin/kwasm" \
-          #       --replace-warn "git rev-parse --short HEAD" 'echo ${self.rev or "dirty"}'
-          #   '';
-          # };
+            enableParallelBuilding = true;
 
-          # kwasm = prev.stdenv.mkDerivation {
-          #   inherit src version;
+            buildPhase = ''
+              XDG_CACHE_HOME=$(pwd) kdist -v build -j4
+            '';
 
-          #   pname = "kwasm";
-
-          #   buildInputs = with prev; [
-          #     final.kwasm-unwrapped
-          #     k-framework.packages.${system}.k
-          #     python310
-          #     which
-          #     z3
-          #   ];
-
-          #   nativeBuildInputs = [ prev.makeWrapper ];
-
-          #   buildPhase = ''
-          #     make build -j$NIX_BUILD_CORES
-          #   '';
-
-          #   installPhase = ''
-          #     mkdir -p "$out/lib/kwasm"
-          #     cp -r .build/* "$out/lib/kwasm"
-          #     makeWrapper ${final.kwasm-unwrapped}/bin/kwasm "$out/bin/kwasm" \
-          #       --run 'export USAGE_NAME=$0' \
-          #       --set KWASM_DEFN_DIR "$out/lib/kwasm/defn" \
-          #       --prefix PATH : ${
-          #         prev.lib.makeBinPath [
-          #           k-framework.packages.${prev.system}.k
-          #           prev.python310
-          #           prev.which
-          #           prev.z3
-          #         ]
-          #       }
-          #   '';
-          # };
+            installPhase = ''
+              mkdir -p $out
+              cp -r ./kdist-*/* $out/
+              mkdir -p $out/bin
+              makeWrapper ${final.kwasm-pyk}/bin/kwasm $out/bin/kwasm \
+                --prefix PATH : ${
+                  prev.lib.makeBinPath [
+                    prev.which
+                    k-framework.packages.${prev.system}.k
+                  ]
+                } \
+                --set KDIST_DIR $out
+            '';
+          };
 
           kwasm-pyk = poetry2nix.mkPoetryApplication {
             python = nixpkgs-pyk.python310;
             projectDir = ./pykwasm;
+            src = rv-utils.lib.mkPykAppSrc {
+              pkgs = import nixpkgs { system = prev.system; };
+              src = ./pykwasm;
+              cleaner = poetry2nix.cleanPythonSources;
+            };
             overrides = poetry2nix.overrides.withDefaults
             (finalPython: prevPython: {
-              pyk = pyk.packages.${prev.system}.pyk-python310;
+              pyk = nixpkgs-pyk.pyk-python310;
+              xdg-base-dirs = prevPython.xdg-base-dirs.overridePythonAttrs
+                (old: {
+                  propagatedBuildInputs = (old.propagatedBuildInputs or [ ])
+                    ++ [ finalPython.poetry ];
+                });
               py-wasm = prevPython.py-wasm.overridePythonAttrs
                 (
                   old: {
@@ -103,30 +95,38 @@
             checkGroups = [ ];
           };
 
-          # kwasm-test = prev.stdenv.mkDerivation {
-          #   inherit src version;
+          kwasm-test = prev.stdenv.mkDerivation {
+            inherit version;
 
-          #   pname = "kwasm-test";
+            pname = "kwasm-test";
+            src = final.kwasm.src;
 
-          #   buildInputs = with prev; [
-          #     final.kwasm
-          #     which
-          #     git
-          #   ];
+            buildInputs = with prev; [
+              final.kwasm
+              final.kwasm-pyk
+              which
+              git
+            ];
 
-          #   buildPhase = ''
-          #     make \
-          #       -j$NIX_BUILD_CORES \
-          #       TEST=${final.kwasm}/bin/kwasm \
-          #       KWASM_DIR=. \
-          #       DEFN_DIR="${final.kwasm}/lib/kwasm/defn" \
-          #       test-conformance-parse test-simple test-conformance-supported test-prove
-          #   '';
+            patchPhase = ''
+              substituteInPlace Makefile \
+                --replace-fail '$(TEST)' '${final.kwasm}/bin/kwasm' \
+                --replace-fail '$(KDIST)' '${nixpkgs-pyk.pyk-python310}/bin/kdist' \
+                --replace-fail '$(SOURCE_DIR)' '${final.kwasm}/wasm-semantics/source'
+            '';
 
-          #   installPhase = ''
-          #     touch $out
-          #   '';
-          # };
+            buildPhase = ''
+              export XDG_CACHE_HOME=$(pwd)
+              make \
+                NIX=1 \
+                -j$NIX_BUILD_CORES \
+                test-conformance-parse test-simple test-conformance-supported test-prove
+            '';
+
+            installPhase = ''
+              touch $out
+            '';
+          };
         });
     in flake-utils.lib.eachSystem [
       "x86_64-linux"
@@ -141,8 +141,8 @@
         };
       in {
         packages = rec {
-          inherit (pkgs) kwasm-pyk;
-          default = kwasm-pyk;
+          inherit (pkgs) kwasm kwasm-pyk kwasm-test;
+          default = kwasm;
         };
     }) // {
       overlays.default = overlay;
