d711996986c88397014897dcd0e8beb65d86fd59
devops <devops@runtimeverification.com>
2024-05-06 19:31:21 +0000

    flake.{nix,lock}: update Nix derivations

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "K Semantics of WebAssembly";
 
   inputs = {
-    k-framework.url = "github:runtimeverification/k/v7.0.43";
+    k-framework.url = "github:runtimeverification/k/v7.0.44";
     nixpkgs.follows = "k-framework/nixpkgs";
     flake-utils.follows = "k-framework/flake-utils";
     rv-utils.follows = "k-framework/rv-utils";
-    pyk.url = "github:runtimeverification/k/v7.0.43?dir=pyk";
+    pyk.url = "github:runtimeverification/k/v7.0.44?dir=pyk";
     nixpkgs-pyk.follows = "pyk/nixpkgs";
     poetry2nix.follows = "pyk/poetry2nix";
   };
