e37b235dae4ecc0f9265029039f3ca33c1d158d2
rv-jenkins <admin@runtimeverification.com>
2024-05-24 07:03:32 -0600

    Update dependency: deps/k_release (#635)
    * deps/k_release: Set Version 7.0.89
    
    * Set Version: 0.1.55
    
    * pykwasm/: sync poetry files v7.0.89
    
    * flake.{nix,lock}: update Nix derivations
    
    * deps/k_release: Set Version 7.0.90
    
    * pykwasm/: sync poetry files v7.0.90
    
    * flake.{nix,lock}: update Nix derivations
    
    * deps/k_release: Set Version 7.0.91
    
    * pykwasm/: sync poetry files v7.0.91
    
    * flake.{nix,lock}: update Nix derivations
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "K Semantics of WebAssembly";
 
   inputs = {
-    k-framework.url = "github:runtimeverification/k/v7.0.88";
+    k-framework.url = "github:runtimeverification/k/v7.0.91";
     nixpkgs.follows = "k-framework/nixpkgs";
     flake-utils.follows = "k-framework/flake-utils";
     rv-utils.follows = "k-framework/rv-utils";
-    pyk.url = "github:runtimeverification/k/v7.0.88?dir=pyk";
+    pyk.url = "github:runtimeverification/k/v7.0.91?dir=pyk";
     nixpkgs-pyk.follows = "pyk/nixpkgs";
     poetry2nix.follows = "pyk/poetry2nix";
   };
