5a4dbd1a4d7f01230cf141f4a37ba30697417b0b
Guy Repta <50716988+gtrepta@users.noreply.github.com>
2024-06-19 14:21:57 -0500

    try to fix hatchling issue

--- flake.nix
+++ flake.nix
@@ -80,6 +80,10 @@
             overrides = poetry2nix.overrides.withDefaults
             (finalPython: prevPython: {
               pyk = nixpkgs-pyk.pyk-python310;
+              pygments = prevPython.pygments.overridePythonAttrs ( old: {
+                buildInputs = (old.buildInputs or [ ])
+                  ++ [ prevPython.hatchling ];
+              });
               xdg-base-dirs = prevPython.xdg-base-dirs.overridePythonAttrs
                 (old: {
                   propagatedBuildInputs = (old.propagatedBuildInputs or [ ])
