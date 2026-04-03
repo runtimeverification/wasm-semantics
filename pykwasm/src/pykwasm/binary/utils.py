111360377871cf3541474d189eaa68b8bd364316
Burak Bilge Yalcinkaya <bbyalcinkaya@gmail.com>
2026-04-03 14:09:46 +0300

    add binary parser for numbers

---
+++ pykwasm/src/pykwasm/binary/utils.py
@@ -0,0 +1,53 @@
+from typing import BinaryIO, Callable, TypeAlias, TypeVar
+
+InputStream: TypeAlias = BinaryIO
+
+A = TypeVar('A')
+Parser: TypeAlias = Callable[[InputStream], A]
+NParser: TypeAlias = Callable[[int, InputStream], A]
+
+
+def read_bytes(n: int, s: InputStream) -> bytes:
+    b = s.read(n)
+    if len(b) != n:
+        raise WasmEOFError(f'Unexpected EOF. Expected {n} bytes, got {len(b)}')
+    return b
+
+
+def read_byte(s: InputStream) -> int:
+    return read_bytes(1, s)[0]
+
+
+def peek_bytes(n: int, s: InputStream) -> bytes:
+    pos = s.tell()
+    b = s.read(n)
+    if len(b) != n:
+        raise WasmEOFError(f'Unexpected EOF. Expected {n} bytes, got {len(b)}')
+    reset(pos, s)
+    return b
+
+
+def peek_byte(s: InputStream) -> int:
+    return peek_bytes(1, s)[0]
+
+
+def skip(n: int, s: InputStream) -> None:
+    s.seek(n, 1)
+
+
+def reset(n: int, s: InputStream) -> None:
+    s.seek(n, 0)
+
+
+def expect_bytes(expected: bytes, s: InputStream) -> None:
+    bs = read_bytes(len(expected), s)
+    if bs != expected:
+        raise WasmParseError(f'Expected {expected!r}, got {bs!r}')
+
+
+class WasmParseError(Exception):
+    pass
+
+
+class WasmEOFError(WasmParseError):
+    pass
